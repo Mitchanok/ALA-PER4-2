66d88d56529732b2e4c3078690fa7a6167dbd714
pootsN <6022703@mborijnland.nl>
2022-06-23 13:42:01 +0200

    ksg

--- artiesten.php
+++ artiesten.php
@@ -9,8 +9,8 @@
       <meta name="author" content="ALA groep 4">
       <meta name="keywords" content="">
       <title>Flavoury</title>
-   <link rel="stylesheet" href="css/artiesten.css">
    <link rel="stylesheet" href="css/navbar.css">
+   <link rel="stylesheet" href="css/artiesten2.css">
    </head>
    <body>
       <article class="main">
@@ -32,7 +32,7 @@
                   <i class="fas fa-caret-down"></i>
                   </a>
                   <ul>
-                     <li><a href="artiesten.html">artiesten</a></li>
+                     <li><a href="artiesten.php">artiesten</a></li>
                   </ul>
                </li>
                <li>
@@ -53,16 +53,24 @@
  <?php
 require('dbconnect.php')
 ?>
-<article class="artiesten">
+<section class="artiesten">
+   <h1 class="title">artiesten</h1>
   <?php
     $sql = "SELECT * FROM artiesten";
     if($result = $conn->query($sql)){
         while ($row = $result->fetch_row()){
-            echo $row[1]."<br> <br>";
+            echo "<h4>" . $row[1]. "</h4>" ."<br> <br>";
            }
         $result->close();
     }
     
     
     ?>
-</article>
+</section>
+<footer class="Footer">
+        <img class="telefoon" src="images/phone_icon_clip_art.jpg" alt="telefoon" height="20px"><p class="footerp">0653177832</p>
+        <p class="copyright">copyright flavoury©</p>
+        <img class="Email" src="images/download.png" alt="mail" height="20px"><p>flavoury@gmail.com</p>           
+     </footer>
+   </body>
+   </html>
